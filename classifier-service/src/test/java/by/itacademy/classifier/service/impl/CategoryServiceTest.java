a777771061179d2fb0c862346c198a8d11e821c5
Artyom Borisov <booorisov@mail.ru>
2023-02-28 12:26:33 +0300

    add tests

---
+++ classifier-service/src/test/java/by/itacademy/classifier/service/impl/CategoryServiceTest.java
@@ -0,0 +1,153 @@
+package by.itacademy.classifier.service.impl;
+
+import by.itacademy.classifier.dao.CategoryRepository;
+import by.itacademy.classifier.dao.entity.CategoryEntity;
+import by.itacademy.classifier.dto.Category;
+import by.itacademy.classifier.utils.Generator;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class CategoryServiceTest {
+
+    @Mock
+    private CategoryRepository repository;
+    @Mock
+    private ConversionService conversionService;
+    @Mock
+    private Generator generator;
+    @InjectMocks
+    private CategoryService categoryService;
+
+    private Category categoryForSaving;
+    private Category categoryFromDB;
+    private CategoryEntity categoryEntity;
+
+    private final LocalDateTime dateTime = LocalDateTime.now();
+    private final UUID existingId = UUID.randomUUID();
+    private final UUID nonexistentId = UUID.randomUUID();
+    private final Pageable pageable = Pageable.ofSize(20).withPage(0);
+
+    @BeforeEach
+    public void createObjects() {
+        final String title = "Книги";
+        categoryForSaving = Category.Builder.createBuilder()
+                .setTitle(title)
+                .build();
+
+        categoryFromDB = Category.Builder.createBuilder()
+                .setId(existingId)
+                .setDtCreate(dateTime)
+                .setDtUpdate(dateTime)
+                .setTitle(title)
+                .build();
+
+        categoryEntity = CategoryEntity.Builder.createBuilder()
+                .setId(existingId)
+                .setDtCreate(dateTime)
+                .setDtUpdate(dateTime)
+                .setTitle(title)
+                .build();
+    }
+
+    @DisplayName("Method \"Category create(Category category)\"")
+    @Test
+    public void givenCategory_whenCreateCategory_thenReturnCategory() {
+        given(generator.generateUUID())
+                .willReturn(existingId);
+        given(generator.now())
+                .willReturn(dateTime);
+        given(conversionService.convert(categoryForSaving, CategoryEntity.class))
+                .willReturn(categoryEntity);
+        given(conversionService.convert(categoryEntity, Category.class))
+                .willReturn(categoryFromDB);
+        given(repository.save(categoryEntity))
+                .willReturn(categoryEntity);
+
+        Category createdCategory = categoryService.create(categoryForSaving);
+
+        assertNotNull(createdCategory);
+        assertEquals(categoryFromDB, createdCategory);
+    }
+
+    @DisplayName("Method \"Page<Category> get(Pageable pageable)\": NOT EMPTY page")
+    @Test
+    public void givenPageable_whenGet_thenReturnPage() {
+        Page<CategoryEntity> entities = getPageWithEntity(categoryEntity);
+        given(repository.findByOrderByTitle(pageable))
+                .willReturn(entities);
+        given(conversionService.convert(categoryEntity, Category.class))
+                .willReturn(categoryFromDB);
+
+        Page<Category> actual = categoryService.get(pageable);
+
+        Page<Category> expected = getPageWithCategory(categoryFromDB);
+        assertEquals(expected, actual);
+    }
+
+    @DisplayName("Method \"Page<Category> get(Pageable pageable)\": EMPTY page")
+    @Test
+    public void givenPageable_whenGet_thenReturnEmptyPage() {
+        Page<CategoryEntity> entities = getEmptyPageWithEntity();
+        given(repository.findByOrderByTitle(pageable))
+                .willReturn(entities);
+
+        Page<Category> actual = categoryService.get(pageable);
+
+        Page<Category> expected = getEmptyPageWithCategory();
+        assertEquals(expected, actual);
+    }
+
+    @DisplayName("Method \"Category get(UUID uuid)\": EXISTENT uuid")
+    @Test
+    public void givenExistentCategoryId_whenGetById_thenReturnCategory() {
+        given(repository.findById(existingId))
+                .willReturn(Optional.of(categoryEntity));
+        given(conversionService.convert(categoryEntity, Category.class))
+                .willReturn(categoryFromDB);
+
+        Category category = categoryService.get(existingId);
+
+        assertEquals(categoryFromDB, category);
+    }
+
+    @DisplayName("Method \"Category get(UUID uuid)\": NONEXISTENT uuid")
+    @Test
+    public void givenNonexistentCategoryId_whenGetById_thenReturnNull() {
+        given(repository.findById(nonexistentId))
+                .willReturn(Optional.empty());
+
+        Category nullCategory = categoryService.get(nonexistentId);
+
+        assertNull(nullCategory);
+    }
+
+    private Page<Category> getPageWithCategory(Category category) {
+        return new PageImpl<>(List.of(category), pageable, 1);
+    }
+
+    private Page<Category> getEmptyPageWithCategory() {
+        return new PageImpl<>(Collections.emptyList(), pageable, 0);
+    }
+
+    private Page<CategoryEntity> getPageWithEntity(CategoryEntity entity) {
+        return new PageImpl<>(List.of(entity), pageable, 1);
+    }
+
+    private Page<CategoryEntity> getEmptyPageWithEntity() {
+        return new PageImpl<>(Collections.emptyList(), pageable, 0);
+    }
+}
---
+++ classifier-service/src/test/java/by/itacademy/classifier/service/impl/CurrencyServiceTest.java
@@ -0,0 +1,186 @@
+package by.itacademy.classifier.service.impl;
+
+import by.itacademy.classifier.dao.CurrencyRepository;
+import by.itacademy.classifier.dao.entity.CurrencyEntity;
+import by.itacademy.classifier.dto.Currency;
+import by.itacademy.classifier.utils.Generator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class CurrencyServiceTest {
+
+    @Mock
+    private CurrencyRepository repository;
+    @Mock
+    private ConversionService conversionService;
+    @Mock
+    private Generator generator;
+    @InjectMocks
+    private CurrencyService currencyService;
+
+    private Currency currencyForSaving;
+    private Currency currencyFromDB;
+    private CurrencyEntity currencyEntity;
+
+    private final LocalDateTime dateTime = LocalDateTime.now();
+    private final UUID existingId = UUID.randomUUID();
+    private final UUID nonexistentId = UUID.randomUUID();
+    private final Pageable pageable = Pageable.ofSize(20).withPage(0);
+
+    @BeforeEach
+    public void createObject() {
+        final String title = "BYN";
+        final String description = "Белорусский рубль";
+
+        currencyForSaving = Currency.Builder.createBuilder()
+                .setTitle(title)
+                .setDescription(description)
+                .build();
+
+        currencyFromDB = Currency.Builder.createBuilder()
+                .setId(existingId)
+                .setDtCreate(dateTime)
+                .setDtUpdate(dateTime)
+                .setTitle(title)
+                .setDescription(description)
+                .build();
+
+        currencyEntity = CurrencyEntity.Builder.createBuilder()
+                .setId(existingId)
+                .setDtCreate(dateTime)
+                .setDtUpdate(dateTime)
+                .setTitle(title)
+                .setDescription(description)
+                .build();
+    }
+
+    @DisplayName("Method \"Currency create(Currency currency)\"")
+    @Test
+    public void givenCurrency_whenCreateCurrency_thenReturnCurrency() {
+        given(generator.generateUUID())
+                .willReturn(existingId);
+        given(generator.now())
+                .willReturn(dateTime);
+        given(conversionService.convert(currencyForSaving, CurrencyEntity.class))
+                .willReturn(currencyEntity);
+        given(conversionService.convert(currencyEntity, Currency.class))
+                .willReturn(currencyFromDB);
+        given(repository.save(currencyEntity))
+                .willReturn(currencyEntity);
+
+        Currency createdCurrency = currencyService.create(currencyForSaving);
+
+        assertNotNull(createdCurrency);
+        assertEquals(currencyFromDB, createdCurrency);
+    }
+
+    @DisplayName("Method \"Page<Currency> get(Pageable pageable)\": NOT EMPTY page")
+    @Test
+    public void givenPageable_whenGet_thenReturnPage() {
+        Page<CurrencyEntity> entities = getPageWithEntity(currencyEntity);
+        given(repository.findByOrderByTitle(pageable))
+                .willReturn(entities);
+        given(conversionService.convert(currencyEntity, Currency.class))
+                .willReturn(currencyFromDB);
+
+        Page<Currency> actual = currencyService.get(pageable);
+
+        Page<Currency> expected = getPageWithCurrency(currencyFromDB);
+        assertEquals(expected, actual);
+    }
+
+    @DisplayName("Method \"Page<Currency> get(Pageable pageable)\": EMPTY page")
+    @Test
+    public void givenPageable_whenGet_thenReturnEmptyPage() {
+        Page<CurrencyEntity> entities = getEmptyPageWithEntity();
+        given(repository.findByOrderByTitle(pageable))
+                .willReturn(entities);
+
+        Page<Currency> actual = currencyService.get(pageable);
+
+        Page<Currency> expected = getEmptyPageWithCurrency();
+        assertEquals(expected, actual);
+    }
+
+    @DisplayName("Method \"Currency get(UUID uuid)\": EXISTENT uuid")
+    @Test
+    public void givenExistentCurrencyId_whenGetById_thenReturnCurrency() {
+        given(repository.findById(existingId))
+                .willReturn(Optional.of(currencyEntity));
+        given(conversionService.convert(currencyEntity, Currency.class))
+                .willReturn(currencyFromDB);
+
+        Currency currency = currencyService.get(existingId);
+
+        assertEquals(currencyFromDB, currency);
+    }
+
+    @DisplayName("Method \"Currency get(UUID uuid)\": NONEXISTENT uuid")
+    @Test
+    public void givenNonexistentCurrencyId_whenGetById_thenReturnNull() {
+        given(repository.findById(nonexistentId))
+                .willReturn(Optional.empty());
+
+        Currency nullCurrency = currencyService.get(nonexistentId);
+
+        assertNull(nullCurrency);
+    }
+
+    private Page<Currency> getPageWithCurrency(Currency currency) {
+        return new PageImpl<>(List.of(currency), pageable, 1);
+    }
+
+    private Page<Currency> getEmptyPageWithCurrency() {
+        return new PageImpl<>(Collections.emptyList(), pageable, 0);
+    }
+
+    private Page<CurrencyEntity> getPageWithEntity(CurrencyEntity entity) {
+        return new PageImpl<>(List.of(entity), pageable, 1);
+    }
+
+    private Page<CurrencyEntity> getEmptyPageWithEntity() {
+        return new PageImpl<>(Collections.emptyList(), pageable, 0);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
